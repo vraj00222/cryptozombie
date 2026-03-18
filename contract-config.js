e8485bb38c63710d6029248c273dd7858f42bd0b
vraj <vrajpatel00222@gmail.com>
2026-03-17 23:10:47 -0700

    Update contract addresses from latest deployment

--- contract-config.js
+++ contract-config.js
@@ -1,5 +1,5 @@
 // Auto-generated after truffle migrate - update these after each deployment
 var CONTRACT_CONFIG = {
-    zombieOwnershipAddress: "0xfaa8542b14dBce7F7Fd3B7D9E29Cec2138a6CCB5",
-    cryptoKittiesAddress: "0xA3a95a97E0745844B4660190bffA703fE40dC0A3"
+    zombieOwnershipAddress: "0x9D616BBA45795289dB2AA8A75905F79e818bB871",
+    cryptoKittiesAddress: "0x48Cf7CA140e0C8A71539830343A59791d51b745b"
 };
